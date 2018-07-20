bab0e71b3bbde0d5d9808dbf3e4b6ba30f1d7b9f
Daniel Chen <team6612@gmail.com>
2018-07-20 16:13:57 +0800

    Create config-template

---
+++ config-template.js
@@ -0,0 +1,14 @@
+module.exports = {
+    line: {
+        channelSecret: "",
+        accessToken: ""
+    },
+    olami: {
+        appKey: "",
+        appSecret: ""
+    },
+    kkbox: {
+        id: "",
+        secret: ""
+    }
+}
